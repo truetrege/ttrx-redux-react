46178dd8049157f88f57df66db932a59d5525c9a
truetrege <mr.trege@gmail.com>
2022-11-28 12:41:30 +0300

    revert public path new logo

---
+++ public/sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html","static/*"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5}); 
