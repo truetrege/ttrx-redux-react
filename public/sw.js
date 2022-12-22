7996bcb2e8eff7df813c30c5a7671ba003794e67
truetrege <mr.trege@gmail.com>
2022-12-22 11:10:29 +0300

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,6 @@
 'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(["index.html","static/*"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5}); 
+importScripts('sw-toolbox.js');
+toolbox.precache(["index.html"]);
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+  
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5}); 
